c4907076dd70f0b32e03b9203f364b042df4c9e6
Ivan Neverovsky <ivan@neverovsky.com.ua>
2020-07-17 17:53:30 +0300

    modified sliders

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,39 @@
-import Slider from './modules/slider';
+import MainSlider from './modules/slider/slider-main';
+import MiniSlider from './modules/slider/slider-mini';
 import VideoPlayer from './modules/playVideo';
 
 window.addEventListener('DOMContentLoaded', () => {
 
-    const slider = new Slider('.page', '.next');
+    const slider = new MainSlider({container: '.page', btns: '.next'});
     slider.render();
+
+    const showUpSlider = new MiniSlider({
+        container: '.showup__content-slider',
+        next: '.showup__prev',
+        prev: '.showup__next',
+        activeClass: 'card-active',
+        animate: true
+    });
+    showUpSlider.init();
+
+    const modulesSlider = new MiniSlider({
+        container: '.modules__content-slider',
+        next: '.modules__info-btns .slick-prev',
+        prev: '.modules__info-btns .slick-next',
+        activeClass: 'card-active',
+        animate: true,
+        autoplay: true
+    });
+    modulesSlider.init();
+
+    const feedSlider = new MiniSlider({
+        container: '.feed__slider',
+        next: '.feed__slider .slick-prev',
+        prev: '.feed__slider .slick-next',
+        activeClass: 'feed__item-active'
+
+    });
+    feedSlider.init();
 
     const videoPlayer = new VideoPlayer('.showup .play', '.overlay');
     videoPlayer.init();
